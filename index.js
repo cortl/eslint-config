2d36f8080191b31c5b245c4ce6557b59abebd596
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-02-13 09:36:43 -0600

    basic node rules

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-module.exports = {
-    rules: {
-        semi: [2, "always"]
-    }
-}
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+const {mergeAndConcat} = require('merge-anything');
+
+const node = require('./src/node');
+
+const config = mergeAndConcat(
+    node
+)
+
+module.exports = config
